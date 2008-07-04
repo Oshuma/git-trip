53cc4488b5e14904ca3c2d5bc06762feceb621dc
Dale Campbell <dale@save-state.net>
2008-07-03 21:14:01 -0500

    Reworking Painter a bit.

--- lib/git_trip/painter.rb
+++ lib/git_trip/painter.rb
@@ -1,35 +1,28 @@
 module GitTrip
 
-  # This does the work of creating a commit specific image(s).
-  # Takes either a single SHA string <b>OR</b> an array of commit SHAs.
+  # This does the work of creating a commit specific image.
+  # Takes single 40 character SHA string.
   class Painter
     include Magick
 
-    def initialize(data)
-      raise Errors::RTFM unless (data.is_a?(Array) || data.is_a?(String))
-      @data = validate(data)
+    def initialize(sha)
+      raise Errors::RTFM unless sha.is_a?(String)
+      raise Errors::InvalidSHA if invalid_sha?(sha)
+      @sha = sha
     end
 
-    # Returns an array of RGB color codes.
-    # def colors; end
+    # Returns a 5 element array of RGB color codes.
+    def colors
+      array = []
+      all   = @sha.split('')
+      5.times { array << all.slice!(0, 8).to_s }
+      return array
+    end
 
     private
 
-    # Ensures +data+ contains either a SHA string or an array of SHAs.
-    # Returns +data+ if valid.
-    def validate(data)
-      if data.is_a? Array
-        data.each { |sha| raise Errors::InvalidSHA if invalid_sha(sha) }
-      else # must be a String
-        raise Errors::InvalidSHA if invalid_sha(data)
-      end
-      # No Errors raised, return the data.
-      return data
-    end
-
     # Returns true if +sha+ is invalid.
-    def invalid_sha(sha)
-      raise Errors::InvalidSHA unless sha.is_a? String
+    def invalid_sha?(sha)
       sha.size != 40
     end
   end # of Painter
--- spec/git_trip/painter_spec.rb
+++ spec/git_trip/painter_spec.rb
@@ -1,33 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
-describe 'common painter', :shared => true do
-  it "should raise Errors::RTFM" do
-    lambda { Painter.new(5) }.should raise_error(Errors::RTFM)
-  end
-end
-
-describe Painter, 'with array' do
-  it_should_behave_like 'common painter'
-
-  before(:each) do
-    @commits = %w{
-      ruby is fucking awesome word to your mother
-    }.map { |text| Digest::SHA1.hexdigest(text) }
-    @painter = Painter.new(@commits)
-  end
-
-  it "should instantiate with an array of commits" do
-    @painter.should be_instance_of(Painter)
-  end
-
-  it "should raise Errors::InvalidSHA" do
-    lambda { Painter.new(%w{ not a valid sha }) }.should raise_error(Errors::InvalidSHA)
-  end
-end
-
-describe Painter, 'with a single commit' do
-  it_should_behave_like 'common painter'
-
+describe Painter do
   before(:each) do
     @commit  = Digest::SHA1.hexdigest('ruby')
     @painter = Painter.new(@commit)
@@ -41,7 +14,21 @@ describe Painter, 'with a single commit' do
     lambda { Painter.new('not a valid sha') }.should raise_error(Errors::InvalidSHA)
   end
 
+  it "should raise Errors::RTFM" do
+    lambda { Painter.new(5) }.should raise_error(Errors::RTFM)
+  end
+
   it "should return an array of 5 RGB color codes" do
-    @painter.colors(@commit).size.should eql(5)
+    @painter.colors.size.should eql(5)
+  end
+
+  it "should have 8 characters per color" do
+    @painter.colors.each do |color|
+      color.size.should eql(8)
+    end
+  end
+
+  it "should have proper colors for it's SHA" do
+    @painter.colors.join.should eql(@commit)
   end
 end
